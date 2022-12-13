fa9a86a52235aae5f4d386f0563f3ac3e3509018
Tatiana <Bowmantatiana2@gmail.com>
2022-12-12 17:06:52 -0800

    commit tree eab9e72fab482f710ea8bc7f7d55d1ee5ea02ea3

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let distance = 0;
+    while (someValue !== 42) {
+        if (someValue < 42) {
+            someValue++;
+            distance++;
+        } else {
+            someValue--;
+            distance++;
+        }
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(someValue) {
+    let feet = distanceFromHqInBlocks(someValue) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceTraveled = 0;
+    while (start !== destination) {
+        if (start < destination) {
+            start++;
+            distanceTraveled += 264;
+        } else {
+            start--;
+            distanceTraveled += 264;
+        }
+    }
+    return distanceTraveled;
+}
+
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0;
+    }
+
+    else if (feet >= 400 && feet <= 2000) {
+        let total = feet - 400;
+        return total * 0.02;
+    }
+
+    else if (feet > 2000 && feet < 2500) {
+        return 25;
+    }
+
+     else if(feet > 2500) {
+        return 'cannot travel that far';
+    }
+
+}
